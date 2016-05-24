a17a4439779dc5597815181be58677b0de843457
Julien Osmalskyj <josmalsky@ulg.ac.be>
2016-05-24 09:01:45 +0200

    All files are licensed under LGPL v3

--- src/libdataset/ThirdPartyLibManager.cpp
+++ src/libdataset/ThirdPartyLibManager.cpp
@@ -1,6 +1,19 @@
-/**
- * Copyright - Benjamin Laugraud - 2015
- */
+// Copyright (C) 2016   Benjamin Laugraud
+//
+// This file is part of LibDataset.
+//
+// LibDataset is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// LibDataset is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with LibDataset.  If not, see http://www.gnu.org/licenses.
 #include <libxml/parser.h>
 #include <libxml/dict.h>
 
